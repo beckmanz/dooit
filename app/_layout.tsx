a420f7861c42220c11b2b313bfabcd40a3fc72df
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-02-07 19:51:22 -0300

    feat: cria contexto para verificar e armazenar a sesão do usuário

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
-import { Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import { router, Stack } from 'expo-router'
+import { AuthProvider, useAuth } from '../contexts/AuthContext'
+import { supabase } from '@/lib/supabase'
 
-export default function MainLayout() {
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  )
+}
+
+function MainLayout() {
+
+  const { setAuth } = useAuth()
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+
+      if (session) {
+        setAuth(session.user)
+        router.replace('/(panel)/home/page')
+        return;
+      }
+
+      setAuth(null)
+      router.replace('/')
+
+    })
+  }, [])
+
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
